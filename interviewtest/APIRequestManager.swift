77176f8ebf2e6ccf64ee51fd237177e781e2cf8e
Terry Bu <terrybu88@gmail.com>
2020-05-09 11:25:56 -0400

    Gitignore fixed. API Request Manager created

---
+++ interviewtest/APIRequestManager.swift
@@ -0,0 +1,18 @@
+//
+//  APIRequestManager.swift
+//  interviewtest
+//
+//  Created by Terry bu on 5/9/20.
+//  Copyright © 2020 Terry Bu. All rights reserved.
+//
+
+import Foundation
+
+class APIRequestManager {
+    static let sharedInstance = APIRequestManager()
+    //In Swift globals, including static members of classes are lazy by default so this is correct:
+
+     init() {
+        //TODO: Finish initializing and implement methods to call the JSON API 
+     }
+}
