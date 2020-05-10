414d6bfa20190cb472e800f16834d775405a3a74
Terry Bu <terrybu88@gmail.com>
2020-05-10 10:09:17 -0400

    cleanup

--- SwiftiOSCodableTableView/APIRequestManager.swift
+++ SwiftiOSCodableTableView/APIRequestManager.swift
@@ -12,13 +12,9 @@ class APIRequestManager {
     static let sharedInstance = APIRequestManager()
     //In Swift globals, including static members of classes are lazy by default so this is correct
     let api_url = URL(string: "http://api.open-notify.org/astros.json")!
-    let encoder = JSONEncoder()
-    let decoder = JSONDecoder()
     var delegate: APIRequestManagerDelegate?
     
      init() {
-        //TODO: Finish initializing and implement methods to call the JSON API
-        //list of name, spacecraft
         let session = URLSession.shared
         let task = session.dataTask(with: api_url, completionHandler: { data, response, error in
             if let data = data {
@@ -33,6 +29,7 @@ class APIRequestManager {
      }
     
     func parseJsonDataIntoDecodedResponse(data: Data) -> SpaceCraftResponse? {
+        let decoder = JSONDecoder()
         do {
             let spaceCraftResponse = try decoder.decode(SpaceCraftResponse.self, from: data)
             return spaceCraftResponse
